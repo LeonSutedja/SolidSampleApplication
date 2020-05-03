88e4424ab0cec1f85046d84fbe4ca6119afa7411
Leon Sutedja <leon.sutedja@gmail.com>
2020-05-03 14:44:51 +1000

    Added reporting api.
    First step to add masstransit for the reporting read model

---
+++ SolidSampleApplication.Reporting.Api/Dashboard/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SolidSampleApplication.Infrastructure.ApplicationBus;
+using System.Threading.Tasks;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SolidSampleApplication.Reporting.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DashboardController : Controller
+    {
+        private readonly IApplicationBusService _bus;
+
+        public DashboardController(IApplicationBusService bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return (await _bus.Send(new GetMembershipsReportQuery())).ActionResult;
+        }
+    }
+}
---
+++ SolidSampleApplication.Reporting.Api/Dashboard/GetMembershipsReportQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolidSampleApplication.Infrastructure.ApplicationBus;
+using SolidSampleApplication.Infrastructure.Shared;
+using SolidSampleApplication.ReportingReadModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SolidSampleApplication.Reporting.Api.Controllers
+{
+    public class GetMembershipsReportQuery : IQuery<DefaultResponse>
+    {
+    }
+
+    public class GetMembershipsReportQueryHandler : IQueryHandler<GetMembershipsReportQuery, DefaultResponse>
+    {
+        private readonly ReportingReadModelDbContext _context;
+
+        public GetMembershipsReportQueryHandler(ReportingReadModelDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<DefaultResponse> Handle(GetMembershipsReportQuery request, CancellationToken cancellationToken)
+            => DefaultResponse.Success(await _context.MembershipPointsReporting.ToListAsync());
+    }
+}
