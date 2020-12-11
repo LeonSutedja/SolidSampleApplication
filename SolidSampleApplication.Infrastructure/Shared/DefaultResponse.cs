37cf74198ca9538d828e431d6c4340b6a1b9ad33
Leon Sutedja <leon.sutedja@gmail.com>
2020-12-12 05:38:47 +1100

    Added error object to the DefaultResponse
    Fixes DefaultResponse error to show the actual error messages

--- SolidSampleApplication.Infrastructure/Shared/DefaultResponse.cs
+++ SolidSampleApplication.Infrastructure/Shared/DefaultResponse.cs
@@ -7,30 +7,51 @@ namespace SolidSampleApplication.Infrastructure.Shared
     {
         public static DefaultResponse Success(object output = null) => new DefaultResponse(new OkObjectResult(output));
 
+        public static DefaultResponse SuccessAsFile(byte[] fileContents)
+            => new DefaultResponse(new FileContentResult(fileContents, "application/pdf"));
+
         public static DefaultResponse Failed(object requestObject, Exception e = null)
         {
             if(e != null)
-                return new DefaultResponse(new BadRequestObjectResult(requestObject), 1, e.Message, false);
+            {
+                var errorObject = new ErrorObject(requestObject, 1, e.Message);
+                return new DefaultResponse(new BadRequestObjectResult(errorObject), false);
+            }
 
-            return new DefaultResponse(new BadRequestObjectResult(requestObject), 1, string.Empty, false);
+            return new DefaultResponse(new BadRequestObjectResult(requestObject), false);
         }
 
         public static DefaultResponse Failed(object requestObject, string message)
         {
-            return new DefaultResponse(new BadRequestObjectResult(requestObject), 1, message, false);
+            var errorObject = new ErrorObject(requestObject, 1, message);
+            return new DefaultResponse(new BadRequestObjectResult(errorObject), false);
         }
 
         public ActionResult ActionResult { get; private set; }
-        public int ErrorId { get; private set; }
-        public string ErrorDescription { get; private set; }
         public bool IsSuccess { get; private set; }
 
-        protected DefaultResponse(ActionResult actionResult, int errorId = -1, string errorDescription = "", bool isSuccess = true)
+        protected DefaultResponse()
+        {
+        }
+
+        protected DefaultResponse(ActionResult actionResult, bool isSuccess = true)
         {
             ActionResult = actionResult ?? throw new ArgumentNullException(nameof(actionResult));
-            ErrorId = errorId;
-            ErrorDescription = errorDescription;
             IsSuccess = isSuccess;
+        }
+
+        public class ErrorObject
+        {
+            public object RequestObject { get; init; }
+            public int ErrorId { get; init; }
+            public string ErrorDescription { get; init; }
+
+            public ErrorObject(object requestObject, int errorId, string errorDescription)
+            {
+                RequestObject = requestObject ?? throw new ArgumentNullException(nameof(requestObject));
+                ErrorId = errorId;
+                ErrorDescription = errorDescription ?? throw new ArgumentNullException(nameof(errorDescription));
+            }
         }
     }
 }
